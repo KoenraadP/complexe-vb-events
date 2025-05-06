cb225fe043e707f106e08030b5818b62b53f78c8
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-05-06 10:02:41 +0200

    Add code for events01

--- events01/frmLocation.Designer.cs
+++ events01/frmLocation.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlLocation = new System.Windows.Forms.Panel();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.pnlLocation.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlLocation
+            // 
+            this.pnlLocation.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlLocation.Controls.Add(this.lblY);
+            this.pnlLocation.Controls.Add(this.lblX);
+            this.pnlLocation.Location = new System.Drawing.Point(77, 81);
+            this.pnlLocation.Name = "pnlLocation";
+            this.pnlLocation.Size = new System.Drawing.Size(152, 88);
+            this.pnlLocation.TabIndex = 0;
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(14, 65);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(13, 13);
+            this.lblY.TabIndex = 1;
+            this.lblY.Text = "0";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(14, 24);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(13, 13);
+            this.lblX.TabIndex = 0;
+            this.lblX.Text = "0";
             // 
             // frmLocation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.pnlLocation);
             this.Name = "frmLocation";
             this.Text = "Location";
+            this.Click += new System.EventHandler(this.frmLocation_Click);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FrmLocation_MouseClick);
+            this.pnlLocation.ResumeLayout(false);
+            this.pnlLocation.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlLocation;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblX;
     }
 }
 
--- events01/frmLocation.cs
+++ events01/frmLocation.cs
@@ -16,5 +16,39 @@ namespace events01
         {
             InitializeComponent();
         }
+
+        private void frmLocation_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FrmLocation_MouseClick(object sender, MouseEventArgs e)
+        {
+            // MouseEventArgs bevatten properties die specifiek te maken hebbenn
+            // met de muis/cursor
+            // in lblX de 'X' positie van muis tonen waar geklikt werd
+            lblX.Text = e.X.ToString();
+            // in lblY de 'Y' positie van muis tonen waar geklikt werd
+            lblY.Text = e.Y.ToString();
+
+            // het panel verplaatsen naar de locatie waar geklikt werd
+            // pnlLocation.Location = new Point(e.X, e.Y);
+
+            // het panel bovenaan in de linkerhoek plaatsen
+            // pnlLocation.Location = new Point(0, 0);
+
+            // het panel onderaan rechts in de hoek plaatsen
+
+            // code hieronder werkt niet volledig juist omdat er bepaalde
+            // elementen van ons programma in de size extra meegerekend worden
+            // bijvoorbeeld de balk bovenaan
+            // int x = Width - pnlLocation.Width;
+            // int y = Height - pnlLocation.Height;
+
+            // clientsize is enkel het effectieve 'werkvlak'
+            int x = ClientSize.Width - pnlLocation.Width;
+            int y = ClientSize.Height - pnlLocation.Height;
+            pnlLocation.Location = new Point(x, y);
+        }
     }
 }
