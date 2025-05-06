cd2013b4056733ea20bbc458ab42ee6b7a2cc4f8
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-05-06 11:16:55 +0200

    Add code for events03

---
+++ events03/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace events03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxCreo = new System.Windows.Forms.PictureBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUpRight = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCreo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxCreo
+            // 
+            this.pbxCreo.Image = global::events03.Properties.Resources.cvocreo_3350104523;
+            this.pbxCreo.Location = new System.Drawing.Point(258, 144);
+            this.pbxCreo.Name = "pbxCreo";
+            this.pbxCreo.Size = new System.Drawing.Size(98, 93);
+            this.pbxCreo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxCreo.TabIndex = 0;
+            this.pbxCreo.TabStop = false;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Enabled = false;
+            this.btnUp.Location = new System.Drawing.Point(82, 28);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.BtnUp_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Enabled = false;
+            this.btnRight.Location = new System.Drawing.Point(152, 57);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.BtnRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Enabled = false;
+            this.btnLeft.Location = new System.Drawing.Point(12, 57);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 3;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Enabled = false;
+            this.btnDown.Location = new System.Drawing.Point(82, 86);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 4;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.BtnDown_Click);
+            // 
+            // btnUpRight
+            // 
+            this.btnUpRight.Enabled = false;
+            this.btnUpRight.Location = new System.Drawing.Point(163, 28);
+            this.btnUpRight.Name = "btnUpRight";
+            this.btnUpRight.Size = new System.Drawing.Size(75, 23);
+            this.btnUpRight.TabIndex = 5;
+            this.btnUpRight.Text = "Up Right";
+            this.btnUpRight.UseVisualStyleBackColor = true;
+            this.btnUpRight.Click += new System.EventHandler(this.BtnUpRight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 381);
+            this.Controls.Add(this.btnUpRight);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.pbxCreo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCreo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxCreo;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUpRight;
+    }
+}
+
---
+++ events03/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace events03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnUp_Click(object sender, EventArgs e)
+        {
+            // 'lange' manier
+
+            // huidige locatie van de picturebox
+            //int x = pbxCreo.Location.X;
+            //int y = pbxCreo.Location.Y;
+
+            // nieuw verticaal locatie punt instellen
+            //y = y - 10;
+
+            // verplaatsen
+            //pbxCreo.Location = new Point(x, y);
+
+
+            // 'korte' manier
+            // Top = de afstand tussen de bovenrand (top)
+            // van de form en de bovenrand van de picturebox
+            // door deze 'kleiner' te maken, komt de picturebox
+            // dichter bij de bovenrand te staan dus gaat hij omhoog
+            pbxCreo.Top -= 10;
+        }
+
+        private void BtnDown_Click(object sender, EventArgs e)
+        {
+            pbxCreo.Top += 10;
+        }
+
+        private void BtnRight_Click(object sender, EventArgs e)
+        {
+            // Left = de afstand tussen de linkerrand (left) van de form
+            // en de linkerrand van de picturebox
+            pbxCreo.Left += 10;
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            // pbxCreo.Left -= 10;
+            // langer alternatief
+            pbxCreo.Left = pbxCreo.Left - 10;
+        }
+
+        private void BtnUpRight_Click(object sender, EventArgs e)
+        {
+            pbxCreo.Top -= 10;
+            pbxCreo.Left += 10;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // als ik op het pijltje omhoog druk...
+            if (e.KeyCode == Keys.Up)
+            {
+                // ...dan moet de picturebox omhoog
+                pbxCreo.Top -= 10;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                pbxCreo.Left += 10;
+            }
+        }
+    }
+}
